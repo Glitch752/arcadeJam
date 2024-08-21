38f22498347596dc0ed52cc0568f63305352cb15
Glitch752 <xxGlitch752xx@gmail.com>
2024-08-21 15:03:50 -0500

    Start making tutorial level and add reset button

---
+++ level_logic.gd
@@ -0,0 +1,15 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _unhandled_input(event):
+	if event.is_action("reset"):
+		get_tree().reload_current_scene()
